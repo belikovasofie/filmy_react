d08cb1bac015a6c4210ac0eca74dc2d7ed4aa5ee
Sofya Belikova <belikovasofie@gmail.com>
2021-06-03 16:13:07 +0200

    search page policko

--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 
 const SearchPage = () => {
-  return <div>Search</div>;
+  const [inputValue, setIputValue] = useState('');
+  const { push } = useHistory();
+
+  const handleChange = (e) => {
+    setIputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    push(`/film-results/${inputValue}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      Search
+      <label>
+        <input onChange={handleChange} value={inputValue} type="text" />
+      </label>
+    </form>
+  );
 };
 
 export default SearchPage;
