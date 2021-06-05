be39ec2f2e13491ab3ef68b86864b9f0f2fb0995
Eva Lastuvkova <eva.lastuvkova@gmail.com>
2021-06-05 11:32:42 +0200

    quotes

---
+++ src/components/Quotes/quotes.js
@@ -0,0 +1,593 @@
+export const quotes = [
+  {
+    id: '1',
+    quote: 'You had me at "hello.',
+    movie: 'Jerry Maguire (1996)',
+  },
+  {
+    id: '2',
+    quote: 'A boy´s best friend is his mother.',
+    movie: 'Psycho (1960)',
+  },
+  {
+    id: '3',
+    quote: 'Greed, for lack of a better word, is good.',
+    movie: 'Wall Street (1987)',
+  },
+
+  {
+    id: '4',
+    quote: 'Keep your friends close, but your enemies closer.',
+    movie: 'The Godfather Part II (1974)',
+  },
+
+  {
+    id: '5',
+    quote: 'Say "hello" to my little friend!',
+    movie: 'Scarface (1983)',
+  },
+  {
+    id: '6',
+    quote: 'Elementary, my dear Watson.',
+    movie: 'The Adventures of Sherlock Holmes (1939)',
+  },
+  {
+    id: '7',
+    quote: 'Take your stinking paws off me, you damned dirty ape.',
+    movie: 'Planet of the Apes (1968)',
+  },
+  {
+    id: '8',
+    quote:
+      'Of all the gin joints in all the towns in all the world, she walks into mine.',
+    movie: 'Casablanca (1942)',
+  },
+  {
+    id: '9',
+    quote: 'Here´s Johnny!',
+    movie: 'The Shining (1980)',
+  },
+  {
+    id: '10',
+    quote: 'Forget it, Jake, it´s Chinatown.',
+    movie: 'Chinatown (1974)',
+  },
+  {
+    id: '11',
+    quote: 'Hasta la vista, baby.',
+    movie: 'Terminator 2:  Judgment Day (1991)',
+  },
+  {
+    id: '12',
+    quote: 'Soylent Green is people!',
+    movie: 'Soylent Green (1973)',
+  },
+  {
+    id: '13',
+    quote: 'Listen to them.  Children of the night.  What music they make.',
+    movie: 'Dracula (1931)',
+  },
+  {
+    id: '14',
+    quote:
+      'Listen to me, mister. You´re my knight in shining armor.  Don´t you forget it.  You´re going to get back on that horse, and I´m going to be right behind you, holding on tight, and away we´re gonna , go, go!',
+    movie: 'On Golden Pond (1981)',
+  },
+  {
+    id: '15',
+    quote: 'A martini.  Shaken, not stirred.',
+    movie: 'Goldfinger (1964)',
+  },
+  {
+    id: '16',
+    quote: 'I feel the need - the need for speed!',
+    movie: 'Top Gun (1986)',
+  },
+  {
+    id: '17',
+    quote: 'Carpe diem.  Seize the day, boys.  Make your lives extraordinary.',
+    movie: 'Dead Poets Society (1989)',
+  },
+  {
+    id: '18',
+    quote: 'Nobody puts Baby in a corner.',
+    movie: 'Dirty Dancing (1987)',
+  },
+  {
+    id: '19',
+    quote: 'I´ll get you, my pretty, and your little dog, too!',
+    movie: 'The Wizard of Oz (1939)',
+  },
+  {
+    id: '20',
+    quote: 'I´m the king of the world!',
+    movie: 'Titanic (1997)',
+  },
+  {
+    id: '21',
+    quote: 'I´m gonna make him an offer he can´t refuse.',
+    movie: 'The Godfather (1972)',
+  },
+  {
+    id: '22',
+    quote: 'Toto, I´ve a feeling we´re not in Kansas anymore.',
+    movie: 'The Wizard of Oz (1939)',
+  },
+  {
+    id: '23',
+    quote: 'Here´s looking at you, kid.',
+    movie: 'Casablanca (1942)',
+  },
+  {
+    id: '24',
+    quote: 'All right, Mr. DeMille, I´m ready for my close-up.',
+    movie: 'Sunset Blvd. (1950)',
+  },
+  {
+    id: '25',
+    quote: 'Love means never having to say you´re sorry.',
+    movie: 'Love Story (1970)',
+  },
+  {
+    id: '26',
+    quote: 'They call me Mister Tibbs!',
+    movie: 'In the Heat of the Night (1967)',
+  },
+  {
+    id: '27',
+    quote: 'Louis, I think this is the beginning of a beautiful friendship.',
+    movie: 'Casablanca (1942)',
+  },
+  {
+    id: '28',
+    quote:
+      'A census taker once tried to test me. I ate his liver with some fava beans and a nice Chianti.',
+    movie: 'The Silence of the Lambs (1991)',
+  },
+  {
+    id: '29',
+    quote: 'Bond. James Bond.',
+    movie: 'Dr. No (1963)',
+  },
+  {
+    id: '30',
+    quote: 'There´s no place like home.',
+    movie: 'The Wizard of Oz (1939)',
+  },
+  {
+    id: '31',
+    quote: 'Show me the money!',
+    movie: 'Jerry Maguire (1996)',
+  },
+  {
+    id: '32',
+    quote: 'After all, tomorrow is another day!',
+    movie: 'Gone with the Wind (1939)',
+  },
+  {
+    id: '33',
+    quote: 'I´ll have what she´s having.',
+    movie: 'When Harry Met Sally... (1989)',
+  },
+  {
+    id: '34',
+    quote: 'You´re gonna need a bigger boat.',
+    movie: 'Jaws (1975)',
+  },
+  {
+    id: '35',
+    quote: 'I´ll be back.',
+    movie: 'The Terminator (1984)',
+  },
+  {
+    id: '36',
+    quote:
+      'My mama always said life was like a box of chocolates. You never know what you´re gonna get.',
+    movie: 'Forrest Gump (1994)',
+  },
+  {
+    id: '37',
+    quote: 'We rob banks.',
+    movie: 'Bonnie and Clyde (1967)',
+  },
+  {
+    id: '38',
+    quote: 'I see dead people.',
+    movie: 'The Sixth Sense (1999)',
+  },
+  {
+    id: '39',
+    quote: 'Well, nobody´s perfect.',
+    movie: 'Some Like It Hot (1959)',
+  },
+  {
+    id: '40',
+    quote: 'It s alive!  It´s alive!',
+    movie: 'Frankenstein (1931)',
+  },
+  {
+    id: '41',
+    quote: 'Houston, we have a problem.',
+    movie: 'Apollo 13 (1995)',
+  },
+  {
+    id: '42',
+    quote: 'There´s no crying in baseball!',
+    movie: 'A League of Their Own (1992)',
+  },
+  {
+    id: '43',
+    quote: 'A boy´s best friend is his mother.',
+    movie: 'Psycho (1960)',
+  },
+  {
+    id: '44',
+    quote: 'Greed, for lack of a better word, is good.',
+    movie: 'Wall Street (1987)',
+  },
+  {
+    id: '45',
+    quote: 'Keep your friends close, but your enemies closer.',
+    movie: 'The Godfather Part II (1974)',
+  },
+  {
+    id: '46',
+    quote: 'Mrs. Robinson, you´re trying to seduce me.  Aren´t you?',
+    movie: 'The Graduate (1967)',
+  },
+  {
+    id: '47',
+    quote: 'Just keep swimming',
+    movie: 'Finding Nemo (2003)',
+  },
+  {
+    id: '48',
+    quote: 'What´s the most you ever lost on a coin toss?',
+    movie: 'No Country for Old Men (2007)',
+  },
+  {
+    id: '49',
+    quote: 'E.T. phone home.',
+    movie: 'E.T. The Extra-Terrestrial (1982)',
+  },
+  {
+    id: '50',
+    quote: 'Ogres are like onions.',
+    movie: 'Shrek (2001)',
+  },
+  {
+    id: '51',
+    quote: 'You have bewitched me, body and soul.',
+    movie: 'Pride and Prejudice (2005)',
+  },
+  {
+    id: '52',
+    quote:
+      'So be prepared, be enthusiastic, and leave your bullshit attitude and baggage at the door because we don´t need it!',
+    movie: 'Wet Hot American Summer (2001)',
+  },
+  {
+    id: '53',
+    quote: 'I just wanted to take another look at you.',
+    movie: 'A Star Is Born (2018)',
+  },
+  {
+    id: '54',
+    quote: 'Put some Windex on it.',
+    movie: 'My Big Fat Greek Wedding (2002)',
+  },
+  {
+    id: '55',
+    quote: 'I want to play a game.',
+    movie: 'Saw (2004)',
+  },
+  {
+    id: '56',
+    quote: 'Florals? For spring? Groundbreaking.',
+    movie: 'The Devil Wears Prada (2006)',
+  },
+  {
+    id: '57',
+    quote: 'I live my life a quarter mile at a time.',
+    movie: 'The Fast and the Furious (2001)',
+  },
+  {
+    id: '58',
+    quote: 'I´m a fiend for mojitos.',
+    movie: 'Miami Vice (2006)',
+  },
+  {
+    id: '59',
+    quote:
+      'Exercise gives you endorphins. Endorphins make you happy. Happy people just don´t shoot their husbands, they just don´t.',
+    movie: 'Legally Blonde (2001)',
+  },
+  {
+    id: '60',
+    quote: 'Look at me. Look at me. I am the captain now.',
+    movie: 'Captain Phillips (2013)',
+  },
+  {
+    id: '61',
+    quote: 'If you´re a bird, I´m a bird.',
+    movie: 'The Notebook (2004)',
+  },
+  {
+    id: '62',
+    quote: 'What is this? A center for ants?',
+    movie: 'Zoolander (2001)',
+  },
+  {
+    id: '63',
+    quote: 'Honey? Where´s my super suit?',
+    movie: 'The Incredibles (2004)',
+  },
+  {
+    id: '64',
+    quote: 'I´m glad he´s single because I´m going to climb that like a tree.',
+    movie: 'Bridesmaids (2011',
+  },
+  {
+    id: '65',
+    quote:
+      'I don´t like sand. It´s all coarse, and rough, and irritating. And it gets everywhere.',
+    movie: 'Star Wars: Episode II – Attack of the Clones (2002)',
+  },
+  {
+    id: '66',
+    quote: 'Even artichokes have hearts.',
+    movie: 'Amélie (2001)',
+  },
+  {
+    id: '67',
+    quote: 'You had my curiosity. But now you have my attention.',
+    movie: 'Django Unchained (2012)',
+  },
+  {
+    id: '68',
+    quote: 'You´re a wizard, ´arry.',
+    movie: 'Harry Potter and the Sorcerer´s Stone (2001)',
+  },
+  {
+    id: '69',
+    quote: 'My tastes are very singular.',
+    movie: 'Fifty Shades of Grey (2015)',
+  },
+  {
+    id: '70',
+    quote: 'To me, you are perfect.',
+    movie: 'Love Actually (2003)',
+  },
+  {
+    id: '71',
+    quote: 'I have to return some videotapes.',
+    movie: 'American Psycho (2000)',
+  },
+  {
+    id: '72',
+    quote: 'I am gonna kill Bill.',
+    movie: 'Kill Bill: Vol. 2 (2004)',
+  },
+  {
+    id: '73',
+    quote: 'I have nipples, Greg. Could you milk me?',
+    movie: 'Meet the Parents (2000)',
+  },
+  {
+    id: '74',
+    quote: 'They called me Mr. Glass.',
+    movie: 'Unbreakable (2000)',
+  },
+  {
+    id: '75',
+    quote:
+      'None of you understand. I´m not locked up in here with YOU. You´re locked up in here with ME.',
+    movie: 'Watchmen (2009)',
+  },
+  {
+    id: '76',
+    quote: 'I’m not bad, I’m just drawn that way.',
+    movie: 'Who Framed Roger Rabbit (1988)',
+  },
+  {
+    id: '77',
+    quote:
+      'Both my wife and daughter think I´m this gigantic loser, and they´re right.',
+    movie: 'American Beauty (1999)',
+  },
+  {
+    id: '78',
+    quote: 'It’s just a flesh wound.',
+    movie: 'Monty Python & The Holy Grail (1975)',
+  },
+  {
+    id: '79',
+    quote: 'I eat Green Berets for breakfast. And right now, I´m very hungry!',
+    movie: 'Commando (1985)',
+  },
+  {
+    id: '80',
+    quote: 'I am double the worst trouble you ever thought of.',
+    movie: 'Heat (1995)',
+  },
+  {
+    id: '81',
+    quote: 'Hliník se odstěhoval do Humpolce!.',
+    movie: 'Marečku, podejte mi pero (1976)',
+  },
+
+  {
+    id: '82',
+    quote: 'Tady jsem si dovolil pár švestiček ze své zahrádky..',
+    movie: 'Marečku, podejte mi pero (1976)',
+  },
+  {
+    id: '83',
+    quote: 'Pane učiteli, už je čas…',
+    movie: 'Marečku, podejte mi pero (1976)',
+  },
+  {
+    id: '84',
+    quote: 'Tady jsem si dovolil pár švestiček ze své zahrádky..',
+    movie: 'Marečku, podejte mi pero (1976)',
+  },
+  {
+    id: '85',
+    quote: 'Kam až sahají Hujerovi?',
+    movie: 'Marečku, podejte mi pero (1976)',
+  },
+  {
+    id: '86',
+    quote: 'Asi dobrej oddíl, no…',
+    movie: 'Jáchyme hoď ho stroje (1974)',
+  },
+  {
+    id: '87',
+    quote: 'Nebudeme se pouštět do žádných větších akcí…',
+    movie: 'Jáchyme hoď ho stroje (1974)',
+  },
+  {
+    id: '88',
+    quote:
+      'Náš ústav se Vám mými ústy omlouvá za toto politováníhodné nedopatření, ke kerému dochází maximálně jedenkrát za 10 let.',
+    movie: 'Jáchyme hoď ho stroje (1974)',
+  },
+  {
+    id: '89',
+    quote: 'Kde udělali soudruzi z NDR chybu?',
+    movie: 'Pelíšky (1999)',
+  },
+  {
+    id: '90',
+    quote: 'Tak z toho nedělejme aféru, onanoval každý...',
+    movie: 'Pupendo (2003)',
+  },
+  {
+    id: '91',
+    quote:
+      'Repráky here, dráty here, kazeťák here... I am going, I am playing.. a to je zakázaný, jo.',
+    movie: 'Pupendo (2003)',
+  },
+  {
+    id: '92',
+    quote:
+      'Nikam neuběgáj a normálně zůstaň v metró... konec hlášení v ruském jazyce.',
+    movie: 'Kolja (1996)',
+  },
+  {
+    id: '93',
+    quote:
+      'Jestlipak si myslíte, soudruzi, že to přežil? No přežil, protože mu to politicky myslelo, vy šmejdi!',
+    movie: 'Černí baroni (1992)',
+  },
+  {
+    id: '94',
+    quote:
+      'Do obrněného transportéru se vejde 20 - 24 západoněmeckých soudruhů.',
+    movie: 'Černí baroni (1992)',
+  },
+  {
+    id: '95',
+    quote: 'Das ganze tschechische Volk ist eine Simulantenbande',
+    movie: 'Dobrý voják Švejk (1956)',
+  },
+  {
+    id: '96',
+    quote: 'Jestli si ten fotbal neprosadíš, tak si u mě mrtvej muž.',
+    movie: 'Ecce Homo Homolka (1969)',
+  },
+  {
+    id: '97',
+    quote: 'Kdo si nenakrad, musí to brát, jako že nevyhrál.',
+    movie: 'Hoří, má panenko (1967)',
+  },
+  {
+    id: '98',
+    quote: 'Máš v hlavně místo mozku z piva kostku.',
+    movie: 'Slunce seno, jahody (1983)',
+  },
+  {
+    id: '99',
+    quote: 'Nečum na mně, nebo ti useknu ruku',
+    movie: 'Jedna ruka netleská (2003)',
+  },
+  {
+    id: '100',
+    quote:
+      'Ráno ho našli na pláži, ležel tam jak vorvaň. Už se do něho pouštěli racci.',
+    movie: 'Účastníci zájezdu (2006)',
+  },
+  {
+    id: '101',
+    quote: 'Slovan jsem a Slovan budu! Až jindy Lakatoši.',
+    movie: 'Obecná škola (1991)',
+  },
+  {
+    id: '102',
+    quote:
+      'Rodiče, kteří mají zájem vidět Idiota, nechť se dostaví do ředitelny.',
+    movie: 'Obecná škola (1991)',
+  },
+  {
+    id: '103',
+    quote: 'Nudíte se? Kupte si medvídka mývala.',
+    movie: 'Postřižiny (1980)',
+  },
+  {
+    id: '104',
+    quote: 'Tati, a bila tě taky tvoje maminka? Ne, jenom ta tvoje.',
+    movie: 'Slunce seno jahody (1983)',
+  },
+  {
+    id: '105',
+    quote:
+      'Já znám široko daleko všechny krávy, ale vás jsem tady ještě neviděl.',
+    movie: 'Léto s kovbojem (1976)',
+  },
+  { id: '106', quote: 'Zavřete oči, odcházím.', movie: 'Kristián (1939)' },
+  {
+    id: '107',
+    quote: 'Vona taky dáma může být pěkná svině...',
+    movie: 'Světáci (1969)',
+  },
+  {
+    id: '108',
+    quote: 'Prosimvás, netykejte mi, ja to vopravdu nemám rád. Kdo ti tyká?',
+    movie: 'Homolka a tobolka (1972)',
+  },
+  {
+    id: '109',
+    quote: 'Co to je za čaj? - Normální houbovej čaj...',
+    movie: 'Jedna ruka netleská (2003)',
+  },
+  {
+    id: '110',
+    quote: 'Sejdeme se za 15 minut - já nemám hodinky - tak za 20.',
+    movie: 'Jedna ruka netleská (2003)',
+  },
+  {
+    id: '111',
+    quote: 'Doktor říkal, že je momentálně zaostalej.',
+    movie: 'Vesničko má středisková (1985)',
+  },
+  {
+    id: '112',
+    quote: 'Chlapi, nelijte to pivo z vokna...',
+    movie: 'Vesničko má středisková (1985)',
+  },
+  {
+    id: '113',
+    quote: 'Nežereš maso, nepoznáš vtip!',
+    movie: 'Knoflíkáři (1997)',
+  },
+  {
+    id: '114',
+    quote: 'Neřeš, nepřepínej a hlavně po ničem nepátrej',
+    movie: 'Knoflíkáři (1997)',
+  },
+  {
+    id: '115',
+    quote: 'Ich habe gesagt. Ich habe gesagt! Ich habe gesagt!!!',
+    movie: 'Škola základ života (1938)',
+  },
+];
