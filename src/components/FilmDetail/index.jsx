f58549b91e74894fd94b54968e880479a8b29cd9
Sofya Belikova <belikovasofie@gmail.com>
2021-06-06 11:02:05 +0200

    info k filmdetail

--- src/components/FilmDetail/index.jsx
+++ src/components/FilmDetail/index.jsx
@@ -1,28 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { sendRequest } from '../../lib/apiService';
+const getGenresTexts = (genres) => {
+  return genres.map((genre) => genre.name).toString();
+};
 
 const FilmDetail = ({ data }) => {
   const [info, setInfo] = useState(null);
 
   useEffect(() => {
     sendRequest(`movie/${data.id}`).then((data) => setInfo(data));
-  }, []);
+  }, [data]);
+  if (!info) return null;
 
   return (
     <div>
       <h2>
-        {data.title}
-        {data.release_date && ` (${data.release_date.slice(0, 4)})`}
-        {data.vote_average}
+        {info.title}
+        {info.release_date && ` (${info.release_date.slice(0, 4)})`}
+        {info.vote_average}
+        {info.genres && getGenresTexts(info.genres)}
+        {info.tagline}
+        {info.origin_country}
       </h2>
       <img
         src={
-          data.poster_path
-            ? `https://image.tmdb.org/t/p/w500${data.poster_path}`
+          info.poster_path
+            ? `https://image.tmdb.org/t/p/w500${info.poster_path}`
             : require('../../img/movie_placeholder.png').default
         }
       />
-      <p>{data.overview}</p>
+      <p>{info.overview}</p>
       {info && (
         <p>
           <a href={`http://imdb.com/title/${info.imdb_id}`}> Odkaz na IMDb</a>
